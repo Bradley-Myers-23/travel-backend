81880d550a84b7fb84bd56f68b50ffebcabd954a
samckinney1 <samuel.mckinney@eagles.oc.edu>
2023-06-11 06:45:34 -0500

    Added admin privileges

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -47,6 +47,7 @@ exports.create = async (req, res) => {
           lastName: req.body.lastName,
           email: req.body.email,
           password: hash,
+          userType: req.body.userType,
           salt: salt,
           userType: "Traveller"
         };
@@ -73,6 +74,7 @@ exports.create = async (req, res) => {
                 firstName: user.firstName,
                 lastName: user.lastName,
                 id: user.id,
+                userType: user.userType,
                 token: token,
               };
               res.send(userInfo);
